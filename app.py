6e6b8c5577ffddbce1e495be9c821cae6c832701
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-10-13 17:36:51 -0500

    Worked on flask video

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template, request  # NOT the same as requests 
+from github_api import get_github_user 
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') # '/' is the home page; this is a route handler
 def homepage():
     return render_template('index.html')
 
+@app.route('/get_user') # route handler will handle requests to the get_user path in the url
+def get_user_info():
+    # get user info from github, and display on new page
+    print('form data is', request.args) # the form data is in variable "request.args"
+    username = request.args.get('username') # will allow you to use the username, will return None if no username
+    
+    user_info, error_message = get_github_user(username)
+    if error_message:
+        return render_template('errors.html', error=error_message)
+    else:  
+        return render_template('github.html', github_info=user_info) # route handler must return something, or None
 
 if __name__ == '__main__':
     app.run()
--- github_api.py
+++ github_api.py
@@ -1,1 +1,42 @@
 # TODO make requests to github API
+
+import requests
+import logging
+
+def get_github_user(username):
+    
+    # 404 errors
+    # other errors
+    # success
+        # return a tuple of: (data, error)
+        # success will: return (data, None)
+        # error will: return (None, error)
+
+    try:
+        response = requests.get(f'https://api.github.com/users/{username}')
+        
+        if response.status_code == 404: # not found
+            return None, f'Username {username} not found'
+        
+        response.raise_for_status()
+
+        response_json = response.json()
+        user_info = extract_user_info(response_json)
+        return user_info, None #return data, error
+    
+    except Exception as er:
+        logging.exception(er)
+        return None, 'Error connecting to Github'
+
+
+def extract_user_info(response_json):
+
+    json_dict = { 
+        'login': response_json.get('login'),
+        'name': response_json.get('name'),
+        'avatar_url': response_json.get('avatar_url'),
+        'home_page': response_json.get('html_url'),
+        'repos': response_json.get('public_repos'),
+    }
+
+    return json_dict
